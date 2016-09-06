f32dfe83f7e3fcfa47f0fbe090899e1840313d96
David Kincheloe Taylor <dktaylor1701@icloud.com>
2016-09-06 19:54:36 -0400

    Initial Commit

---
+++ Nudge/AddNudgeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddNudgeViewController.swift
+//  Nudge
+//
+//  Created by David Taylor on 9/6/16.
+//  Copyright © 2016 Hyper Elephant. All rights reserved.
+//
+
+import UIKit
+
+class AddNudgeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
